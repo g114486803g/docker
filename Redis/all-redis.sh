31a6683199981fa1ee96d06e2d1dbcac24989ae8
g114486803g <48343071+g114486803g@users.noreply.github.com>
2019-06-17 21:11:34 +0800

    Update all-redis.sh

--- Redis/all-redis.sh
+++ Redis/all-redis.sh
@@ -6,10 +6,12 @@ docker tag registry.cn-shenzhen.aliyuncs.com/jbjb/alpine:redis-v5 redis/alpine:v
 docker run --rm -itd -v /db/7000:/var/lib/redis --name master-redis -p7000:6379 redis/alpine:v5.0.5 master
 docker run --rm -itd -v /db/7001:/var/lib/redis --name node1 --link master-redis:master-redis -p7001:6379 redis/alpine:v5.0.5 slave
 
-
+echo "#############################"
 echo "主从密码12300"
 echo "主端口7000 从端口7001"
-
+echo "##############################"
+docker ps -a | grep redis
+echo "   "
 echo "需手动设置从节点加入matser如下: "
 cat << EOF
 # redis-cli -h 172.16.8.244 -p 7001
