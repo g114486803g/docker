a50bb7754f02089e0ceb45d3e2798cb2dcc6b5f7
g114486803g <48343071+g114486803g@users.noreply.github.com>
2020-03-31 13:39:31 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -3,7 +3,7 @@
 yum -y install bash-completion.noarch wget 
 wget https://download.docker.com/linux/centos/docker-ce.repo -P /etc/yum.repos.d/
 yum clean all 
-yum install docker-ce docker-ce-cli containerd.io 
+yum install docker-ce docker-ce-cli containerd.io -y
 systemctl start docker
 
 echo "配置阿里云加速器"
