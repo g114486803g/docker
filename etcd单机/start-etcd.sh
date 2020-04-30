61e833845396503dffc6d976505cb17b26be9152
g114486803g <48343071+g114486803g@users.noreply.github.com>
2020-04-30 13:35:06 +0800

    Create start-etcd.sh

---
+++ etcd单机/start-etcd.sh
@@ -0,0 +1,19 @@
+docker run -d \
+  -p 2379:2379 \
+  -p 2380:2380 \
+  -v /data1/etcd/data:/etcd-data/member \
+  --name etcd \
+   etcd:v3.4.7 \
+  /usr/local/bin/etcd \
+  --name s1 \
+  --data-dir /etcd-data \
+  --listen-client-urls http://0.0.0.0:2379 \
+  --advertise-client-urls http://0.0.0.0:2379 \
+  --listen-peer-urls http://0.0.0.0:2380 \
+  --initial-advertise-peer-urls http://0.0.0.0:2380 \
+  --initial-cluster s1=http://0.0.0.0:2380 \
+  --initial-cluster-token tkn \
+  --initial-cluster-state new \
+  --log-level info \
+  --logger zap \
+  --log-outputs stderr
